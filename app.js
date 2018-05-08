77f6de27a4fd81927b00f6835280f7fc546e0b58
Harrison Chin <harrison_chin@hotmail.com>
2018-05-08 16:14:13 -0700

    Add dotenv package

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalog = require('./routes/catalog');
+require('dotenv').config();
 
 var app = express();
 //Set up mongoose connection
